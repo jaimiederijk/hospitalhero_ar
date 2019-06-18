7b999ae920ae7aa75d1a69d8f63ccd64b092b527
jaimiederijk <jaimiedegiantrijk@gmail.com>
2019-06-19 00:35:23 +0200

    dance stuff

--- public/javascripts/dance.js
+++ public/javascripts/dance.js
@@ -8,16 +8,24 @@ var danceState = {
   points: 0,
   startStop: false,
   danceMoves: ["A","B","C","D"],
-  danceInstructions:["A","D","D","B","B","D","D","B","B"],
-  currentDanceMove: "B",
+  danceInstructions:["A","D","D","B","B","D","D","B","B","D","A"],
+  currentDanceMove: "B"
 
 }
 var setup = () => {
   for (var i = 0; i < danceHtmlElements.danceButtons.length; i++) {
     danceHtmlElements.danceButtons[i].addEventListener("click",(e)=>{
+      //debugger
+    //  console.log(danceState.currentDanceMove + "  " + danceState.danceInstructions);
       if (e.currentTarget.innerHTML == danceState.currentDanceMove) {
-        danceState.points = danceState.points + 1;
-        updatePoints();
+        var currentDanceMove = document.querySelector(".currentdancemove");
+        if (!currentDanceMove.classList.contains("gothim")) {
+          currentDanceMove.classList.add("gothim");
+          danceState.points = danceState.points + 1;
+          updatePoints();
+        } else {// wrong button
+
+        }
       }
     })
   }
@@ -26,27 +34,32 @@ var setup = () => {
 
 var startGame = () => {
   var danceInterval = setInterval( (e)=>{
+    //select next move
     var nr = Math.floor(Math.random() * 4);
     var newDanceMove = danceState.danceMoves[nr];
-
-    danceState.danceInstructions.unshift(newDanceMove);
-    if (danceState.danceInstructions.length>9) {
-      danceState.danceInstructions.pop();
+    //add newmove
+    danceState.danceInstructions.push(newDanceMove);
+    // if longer remove oldest
+    if (danceState.danceInstructions.length>11) {
+      danceState.danceInstructions.shift();
     }
+    // remove oldest from dom
     var updatedEmList = document.querySelectorAll("#danceinstructor em");
     updatedEmList[0].parentNode.removeChild(updatedEmList[0]);
-      //
-      // danceHtmlElements.danceInstructor[0].innerHTML = danceState.danceInstructions[0];
-      // danceHtmlElements.danceInstructor[1].innerHTML = danceState.danceInstructions[1];
-      // danceHtmlElements.danceInstructor[2].innerHTML = danceState.danceInstructions[2];
-      // danceHtmlElements.danceInstructor[3].innerHTML = danceState.danceInstructions[3];
-      // danceHtmlElements.danceInstructor[4].innerHTML = danceState.danceInstructions[4];
-      // danceHtmlElements.danceInstructor[5].innerHTML = danceState.danceInstructions[5];
-      // danceHtmlElements.danceInstructor[6].innerHTML = danceState.danceInstructions[6];
-      // danceHtmlElements.danceInstructor[7].innerHTML = danceState.danceInstructions[7];
-      // danceHtmlElements.danceInstructor[8].innerHTML = danceState.danceInstructions[8];
-    // danceHtmlElements.danceInstructor.innerHTML = danceState.danceInstructions;
+    // add new to dom
+    var newDanceEm = document.createElement("em");
+    var textnode = document.createTextNode(newDanceMove);
 
+    newDanceEm.appendChild(textnode);
+    newDanceEm.classList.add("color"+newDanceMove);
+    newDanceEm.innerHTML = newDanceEm.innerHTML + `<img src="images/lamp.svg" alt="lamp">`;
+    updatedEmList[4].parentNode.appendChild(newDanceEm);
+    //add active class to currentmove
+    updatedEmList[5].classList.add("currentdancemove");
+    updatedEmList[4].classList.remove("currentdancemove");
+    updatedEmList[4].classList.add("movedone");
+    //console.log(danceState.danceInstructions[4]);
+    // debugger
     danceState.currentDanceMove = danceState.danceInstructions[4];
   },2000);
   danceHtmlElements.pause.addEventListener("click",(e)=>{
