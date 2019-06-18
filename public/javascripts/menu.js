28617bb5aab671c1cf1c269e2f3e85137e04b70c
jaimiemac <jaimie@Jaimies-MacBook-Pro.local>
2019-06-18 15:33:03 +0200

    multi markers and figures

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -8,6 +8,7 @@ var htmlElements = {
   menu : document.querySelector('#menu'),
   aScene : document.querySelector('a-scene'),
   ul : document.querySelector('#menu ul'),
+  bMarker : document.querySelector('#bmarker'),
 };
 
 var menuButton = document.querySelector('#menubutton');
@@ -25,7 +26,7 @@ var app = {
 // array of figures that can be captured
 var figures = [
   {
-    name:"Aardvark",
+    name:"Rode Aardvark",
     img:"/images/aardvark.png",
     aentity:`<a-entity
       scale="0.02 0.02 0.02"
@@ -37,18 +38,43 @@ var figures = [
     visableAtMarker:false,
     catched:false,
   },{
-    name:"Dodo",
-    img:"/images/dodo.png",
+    name:"Gele Aardvark",
+    img:"/images/aardvarkyellow.png",
     aentity:`<a-entity
       scale="0.02 0.02 0.02"
       rotation="0 180 -150"
       position="0 0 1"
-      obj-model="obj: url(/images/mybox/tinker.obj);
-      mtl: url(/images/mybox/obj.mtl)">
+      obj-model="obj: url(/images/aardvark_yellow/tinker.obj);
+      mtl: url(/images/aardvark_yellow/obj.mtl)">
     </a-entity>`,
     visableAtMarker:false,
     catched:false,
-  }
+  },{
+    name:"Groene Aardvark",
+    img:"/images/aardvarkgreen.png",
+    aentity:`<a-entity
+      scale="0.02 0.02 0.02"
+      rotation="0 180 -150"
+      position="0 0 1"
+      obj-model="obj: url(/images/aardvark_green/tinker.obj);
+      mtl: url(/images/aardvark_green/obj.mtl)">
+    </a-entity>`,
+    visableAtMarker:false,
+    catched:false,
+  },
+  // {
+  //   name:"Dodo",
+  //   img:"/images/dodo.png",
+  //   aentity:`<a-entity
+  //     scale="0.02 0.02 0.02"
+  //     rotation="0 180 -150"
+  //     position="0 0 1"
+  //     obj-model="obj: url(/images/mybox/tinker.obj);
+  //     mtl: url(/images/mybox/obj.mtl)">
+  //   </a-entity>`,
+  //   visableAtMarker:false,
+  //   catched:false,
+  // }
 ]
 
 var markers = [
@@ -57,8 +83,26 @@ var markers = [
     figureAtMarker:false,
     whichFigure:false,
     pointingAtMarker:false,
+  },
+  {
+    name:"bMarker",
+    figureAtMarker:false,
+    whichFigure:false,
+    pointingAtMarker:false,
   }
 ]
+
+var createFiguresList = () => {
+  var liString = "";
+  for (var i = 0; i < figures.length; i++) {
+    if (figures[i].catched) {
+      liString +=`<li><img class="animalimg" id="aardvarkimg" src="`+ figures[i].img +`" alt="aardvark">
+        <h2>`+ figures[i].name +`</h2>
+      </li>`;
+    }
+  }
+  htmlElements.ul.innerHTML = liString;
+}
 
 var addListeners = () => {
   if (menuButton) {
@@ -76,32 +120,26 @@ var addListeners = () => {
     })
   }
 
-  var createFiguresList = () => {
-    var liString = "";
-    for (var i = 0; i < figures.length; i++) {
-      if (figures[i].catched) {
-        liString +=`<li><img class="animalimg" id="aardvarkimg" src="`+ figures[i].img +`" alt="aardvark">
-          <h2>`+ figures[i].name +`</h2>
-        </li>`;
-      }
-    }
-    htmlElements.ul.innerHTML = liString;
-  }
   // catchsystem
   if (catchButton) {
     catchButton.addEventListener("click",(e)=>{
       // if there is a figure and its being pointed at
-      if (markers[0].figureAtMarker && markers[0].pointingAtMarker) {
-        // which figure has been caught
-        figures[markers[0].whichFigure].catched = true;
-        // set catched to true
-        // add figure to bag
-        createFiguresList();
+      for (var i = 0; i < markers.length; i++) {
+        // markers[i]
+        if (markers[i].figureAtMarker && markers[i].pointingAtMarker) {
+          // which figure has been caught
+          figures[markers[i].whichFigure].catched = true;
+          // set catched to true
+          // add figure to bag
+          createFiguresList();
 
-        // remove 3d figure
-        hiroMarker.innerHTML = "";
-        catchButton.classList.add("nonactive");
+          // remove 3d figure because catched
+          htmlElements[markers[i].name].innerHTML = "";
+          console.log("catch figure");
+          catchButton.classList.add("nonactive");
+        }
       }
+
       // for (var i = 0; i < shadowImages.length; i++) {
       //   shadowImages[i].classList.add("hidden");
       // }
@@ -112,61 +150,94 @@ var addListeners = () => {
   }
   // if marker is in the frame
   if (hiroMarker) {
-    hiroMarker.addEventListener("markerFound", (e)=>{
-      markers[0].pointingAtMarker = true;
-      // console.log("hiro found");
-      if (markers[0].figureAtMarker) {
+    for (var t = 0; t < markers.length; t++) {
+      //add param
+      htmlElements[markers[t].name].markerIndex = t;
+      //marker in frame
+      htmlElements[markers[t].name].addEventListener("markerFound", (e)=>{
+        markers[e.currentTarget.markerIndex].pointingAtMarker = true;
 
-        catchButton.classList.remove("nonactive");
-      }
+        // if there is a figure at the currenmarker
+        if (markers[e.currentTarget.markerIndex].figureAtMarker) {
 
-    })
-    hiroMarker.addEventListener("markerLost", (e)=>{
-      // console.log("hiro lost");
-      markers[0].pointingAtMarker = false;
-      catchButton.classList.add("nonactive");
-    })
+          catchButton.classList.remove("nonactive");
+        }
+
+      })
+
+      htmlElements[markers[t].name].addEventListener("markerLost", (e)=>{
+
+        //no marker in sight
+        markers[e.currentTarget.markerIndex].pointingAtMarker = false;
+        console.log("lose marker");
+        catchButton.classList.add("nonactive");
+      })
+    }
+
   }
 }
 // randomly move figures around the different markers
 var moveFigures = () => {
-  var randomlyMoveAround = () => {
+  var randomlyMoveAround = (figureIndex) => {
     setTimeout((e) => {
+      var foundEmptyMarker = false;
       // if no figure call figure
-      if (hiroMarker.innerText=="") {
-        figureStay(0);
-        console.log("figuresstay called");
+      for (var u = 0; u < markers.length; u++) {
+        // find empty marker
+        if (!markers[u].figureAtMarker) {
+          markers[u].figureAtMarker = true;
+          foundEmptyMarker = true;
+          figureStay(figureIndex, u);
+          console.log("figuresstay "+ figureIndex+ " called on:" + markers[u].name);
+          break;
+        }
       }
-      else {
-        console.log("try move again");
-        randomlyMoveAround();
+      // did not find empty marker
+      if (!foundEmptyMarker) {
+        console.log("try move again "+ figureIndex);
+        randomlyMoveAround(figureIndex);
       }
     },Math.random() * 10000)
   }
 
   // function that sets a figure and then removes it
-  var figureStay = (figureIndex) => {
+  var figureStay = (figureIndex, markerIndex) => {
     // if figure has not been catched
     if (!figures[figureIndex].catched) {
-      hiroMarker.innerHTML = figures[figureIndex].aentity;
-      markers[0].figureAtMarker = true;
-      markers[0].whichFigure = figureIndex;
-      if (markers[0].pointingAtMarker) {
+      htmlElements[markers[markerIndex].name].innerHTML = figures[figureIndex].aentity;
+
+      markers[markerIndex].whichFigure = figureIndex;
+      if (markers[markerIndex].pointingAtMarker) {
         catchButton.classList.remove("nonactive");
       }
       setTimeout((e) =>{
         // after timeout deactivate figure
-        hiroMarker.innerHTML = "";
-        markers[0].figureAtMarker = false;
-        markers[0].whichFigure = false;
-        catchButton.classList.add("nonactive");
-        randomlyMoveAround();
+        console.log(figureIndex +" :F and M: "+markerIndex);
+        htmlElements[markers[markerIndex].name].innerHTML = "";
+        markers[markerIndex].figureAtMarker = false;
+        markers[markerIndex].whichFigure = false;
+        //remove nonactive unless a other figure is visable
+        var otherMarkersWithFigures = false;
+        for (var r = 0; r < markers.length; r++) {
+          // a figure at marker and pointing at marker
+          if (markers[r].figureAtMarker && markers[r].pointingAtMarker) {
+            otherMarkersWithFigures = true;
+          }
+        }
+        if (!otherMarkersWithFigures) {
+          catchButton.classList.add("nonactive");
+        }
+        
+        console.log("try move again after apearence"+ figureIndex);
+        randomlyMoveAround(figureIndex);
       }, 5000)
     }
 
   }
 
-  randomlyMoveAround();
+  randomlyMoveAround(0);
+  randomlyMoveAround(1);
+  randomlyMoveAround(2);
 }
 
 var onStartSetup = () => {
